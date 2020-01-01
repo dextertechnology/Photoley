99c7a843a52e35ce4bb6fa607baeb38474e64e24
diwap <dipipandey1@gmail.com>
2020-01-01 22:13:33 +0545

    file read in chunk and progressbar added

--- photoley/photos/photos.py
+++ photoley/photos/photos.py
@@ -1,6 +1,11 @@
-from photos.get_url import GetUrl
-from utilities.stdout import hash_decorator
+import uuid
+from urllib import request
 
+from photos.get_url import GetUrl
+from utilities.stdout import hash_decorator, PrintProgressBar
+
+
+MEDIA_DIR = '/home/dexter/dextertechnology/Photoley_all/media_cdn'
 
 class Photos:
     def __init__(self, **kwargs):
@@ -11,6 +16,22 @@ class Photos:
     @hash_decorator
     def save(self):
         get_url = GetUrl(self.url, self.args).get_url()
-        
-        print(get_url)
+        uid = str(uuid.uuid4())
+
+        req = request.urlopen(get_url)
+        print(req.headers.get('Content-Length'))
+        with open(f"{MEDIA_DIR}/{uid}.jpg", 'wb') as fp:
+            l = int(req.headers.get('Content-Length'))//1024
+            PrintProgressBar(0, l, prefix= 'Progress:', suffix= 'Complete', length= 50)
+
+            new_range = list(range(0, l))
+            for i, item in enumerate(new_range):
+                chunk = req.read(1024)
+                
+                if not chunk: break
+                fp.write(chunk)
+                PrintProgressBar(i + 1, l, prefix= 'Progress:', suffix= 'Complete', length= 50)
+
+
+        print('\n', get_url)
         return get_url
