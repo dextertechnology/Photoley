d08ef827a119dc18279434fcc58fedf35795ddef
diwap <dipipandey1@gmail.com>
2020-01-09 06:07:33 +0545

    subprocess modified with pipeline

--- photoley/wallpaper/xfce_four/wall_setter.py
+++ photoley/wallpaper/xfce_four/wall_setter.py
@@ -5,6 +5,26 @@ from photoley.photoley.config import MEDIA_DIR
 
 
 def set_wallpaper(img, **kwargs):
+    """
+        XFCE-4
+        =======
+        Introduction
+        ------------
+        In xfce4 based desktop environment, xfconf-query is
+        a commandline utility which is here used for setting
+        desktop wallpaper.
+
+        Check Property
+        --------------
+        To check property, `xfconf-query -c <channel_name> -m`
+        Here, -m is used for monitoring. Try to change desktop
+        wallpaper of the workspace you prefer, then you will get
+        screen propery like:
+        `/backdrop/screen0/monitorDP1/workspace0/last-image`
+        Use that as a property.
+    """
+
+    # TODO: use config file for channel and property
     _property = "/backdrop/screen0/monitorDP1/workspace0/last-image"
     _channel = "xfce4-desktop"
 
@@ -12,14 +32,15 @@ def set_wallpaper(img, **kwargs):
 
     if not os.path.isfile(_img):
         raise Exception("File doesn't exist.")
+
+    command = f"xfconf-query --channel {_channel} \
+        --property {_property} --set {_img}"
     
     print(f"Set image {_img}")
-    return subprocess.call([
-        "xfconf-query",
-        "--channel",
-        kwargs.get("channel", _channel),
-        "--property",
-        kwargs.get("property", _property),
-        "--set",
-        _img
-    ], shell=True, universal_newlines=True)
+    try:
+        shell_cmd = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = shell_cmd.communicate()
+        shell_cmd.wait()
+        return output
+    except Exception as e:
+        return e
